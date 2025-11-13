40d7c2fd3dfcfb024a598e1583099c66a293303c
buldra2000 <buldra00@icloud.com>
2025-11-13 17:43:17 +0100

    Multiple views are supported are the same time

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -3,8 +3,11 @@ package it.unibo.mvc.controller;
 import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
 
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class implements the game controller. It orchestrates the game, exposes methods to its observers
@@ -13,7 +16,8 @@ import java.util.Objects;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private DrawNumberView view;
+    
+    private final List<DrawNumberView> views = new ArrayList<>();
 
     /**
      * Builds a new game controller provided a game model.
@@ -25,19 +29,19 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
     }
 
     @Override
-    public void addView(final DrawNumberView view) {
-        Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
-        }
-        this.view = view;
-        view.setController(this);
-        view.start();
+    public void addView(final DrawNumberView newView) {
+        Objects.requireNonNull(newView, "Cannot set a null view");
+        views.add(newView);
+        newView.setController(this);
+        newView.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
+        DrawResult result = model.attempt(n);
+        for (DrawNumberView v : views) {
+            v.result(result);
+        }
     }
 
     @Override
